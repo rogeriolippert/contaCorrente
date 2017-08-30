83fc55f23295be78e24cc088c9e749feb7a8746a
Ubuntu <ubuntu@alpha13.turing.ieduc.org.br>
2017-08-29 21:44:43 -0300

    implemtação de teste

--- src/br/com/contaCorrente/www/Conta.java
+++ src/br/com/contaCorrente/www/Conta.java
@@ -4,11 +4,16 @@ public class Conta {
 	private String agencia;
 	private String conta;
 	private String titular;
+	private double saldo;
+	private double credito;
+	private double debito;
+	
 	
 	public Conta(String titular, String agencia, String conta) {
 		this.titular = titular;
 		this.setAgencia(agencia);
 		this.setConta(conta);
+		this.setSaldo(0);
 	}
 	
 	public String GetTitular() {
@@ -33,6 +38,29 @@ public class Conta {
 
 	public void setAgencia(String agencia) {
 		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+	public double getCredito() {
+		return credito;
+	}
+	public void setCredito(double credito) {
+		this.credito = credito;
+	}
+	
+	public double getDebito() {
+		return debito;
+	}
+	public void setDebito(double debito) {
+		this.debito = debito;
 	}
 }
 
--- src/test/br/com/contaCorrente/www/TesteConta.java
+++ src/test/br/com/contaCorrente/www/TesteConta.java
@@ -2,23 +2,38 @@ package test.br.com.contaCorrente.www;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import br.com.contaCorrente.www.Conta;
 
 public class TesteConta {
 
-private Conta conta;
+	private Conta conta;
 	
 	@Before
 	public void before() {
-		conta = new Conta("Marcelo", "2-2", "123-4");
+		conta = new Conta("Rogerio", "2-2", "123-4");
 	}
 
 	@Test
-	public void test() {
-		assertEquals(conta.GetTitular(), "Rogerio");
-		assertEquals(conta.GetSaldo(), 0, 0);
+	public void DadosConta() {
+		assertEquals("Rogerio", conta.GetTitular());
+		assertEquals(0, conta.GetSaldo(), 0);
+	}
+	@Test
+	public void DebitoConta() {
+		conta.debito(600);
+		assertEquals(0, conta.GetDebito(-600), 0);
 	}
 
-}
+	@Test
+	public void ContaCredito() {
+		conta.credito(100);
+		assertEquals(100, conta.GetCredito(100), 0);
+	}
+
 
 }
+
+
